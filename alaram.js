46ab8bfa43c68f0524aa87b8044ccf4e48f14224
cypher1002 <mehuldarshni42@gmail.com>
2022-07-24 22:13:58 +0530

    first commit

---
+++ alaram.js
@@ -0,0 +1,59 @@
+let  currentTime =  document.querySelector("h1");
+ const selectMenu =  document.querySelectorAll("select"),
+ content  = document.querySelector(".content");
+ setAlarmBtn = document.querySelector("button");
+ let alarmTime;
+for (let i= 12;i > 0;i--) {
+    i = i< 10 ? "0" +i : i //adding 0  before every number if number is single valued
+   let option = `<option value="${i}">${i}</option>`
+    selectMenu[0].firstElementChild.insertAdjacentHTML("afterend",option)
+}
+
+for (let i= 59;i >=0;i--) {
+    i = i< 10 ? "0" +i : i //adding 0  before every number if number is single valued
+   let option = `<option value="${i}">${i}</option>`
+    selectMenu[1].firstElementChild.insertAdjacentHTML("afterend",option)
+}
+
+for (let i= 2;i > 0;i--) {
+    let ampm = i == 1? "AM" : "PM"
+   let option = `<option value="${ampm}">${ampm}</option>`
+    selectMenu[2].firstElementChild.insertAdjacentHTML("afterend",option)
+}
+
+setInterval(() => {
+    //getting hour min sec
+    let date =  new Date();
+    h = date.getHours();
+    m= date.getMinutes();
+    s= date.getSeconds();
+    ampm = "AM"
+
+    if(h>= 12){
+        h = h-12 ;
+        ampm = "PM";
+    }
+
+    h = h==0 ? h=12 : h; 
+
+    h = h<10? "0" + h:h;
+    m = m<10? "0" + m:m;
+    s = s<10? "0" + s:s;
+
+  currentTime.innerText =`${h}:${m}:${s} ${ampm}`;
+  if(alarmTime == `${h}:${m} ${ampm}`){
+      console.log("alarm ringing......");
+  }
+},1000);
+
+function setAlarm(){
+    let time = `${selectMenu[0].value}:${selectMenu[1].value}:${selectMenu[2].value}:`
+     if(time.includes("Hour")||time.includes("Minute")||time.includes("AM/PM")){
+     return alert("please select valid time to set ");
+    }
+    alarmTime = time;
+    content.classList.add("disable")
+    setAlarmBtn.innerText= "clear alarm"
+}    
+
+ setAlarmBtn.addEventListener("click", setAlarm)
